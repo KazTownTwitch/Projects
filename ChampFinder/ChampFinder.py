3cc63e8dee91c0940a4abc8da8c945b918f7a382
Kaz <kbunker@berkeley.edu>
2020-03-30 03:51:11 -0700

    ChampFinder v1

---
+++ ChampFinder/ChampFinder.py
@@ -0,0 +1,449 @@
+
+def yes(question):
+	print(question)
+	inp = input()
+	return inp == "y" or inp == "yes"
+
+while True:
+	champ = None
+	if yes("Do you want to play with other people?"):
+		if yes("Do you want a champion with at least one ability that you have to aim?"):
+			if yes("Would you sacrifice precious kit damage to protect an ally?"):
+				if yes("Would you give your entire dick for your allies?"):
+					if yes("Have you ever catfished someone?"):
+						champ = "Tahm Kench"
+					else:
+						if yes("Are you into weird puppet people more than actual people?"):
+							champ = "Bard"
+						else:
+							if yes("Is a full head of hair more important than a fabuolous moustache?"):
+								champ = "Taric"
+							else:
+								champ = "Braum"
+				else:
+					if yes("Do you like seeing shit that's moving and stopping it?"):
+						if yes("Do you want an ultimate?"):
+							if yes("Like one where you prance around?"):
+								champ = "Rakan"
+							else:
+								if yes("Okay, how bout one where you blow up fuckin' EVERYONE?"):
+									if yes("And also heal some peeps?"):
+										champ = "Senna"
+									else:
+										champ = "Lux"
+								else:
+									if yes("What about one where you stop the entire enemy team dead in their tracks?"):
+										if yes("While riding on a book?"):
+											champ = "Yuumi"
+										else:
+											champ = "Nami"
+									else:
+										if yes("What about one that PULLS THE WHOLE ENEMY TEAM IN A LITTLE!?!?"):
+											champ = "Orianna"
+										else:
+											if yes("What about one that PUSHES THE WHOLE ENEMY TEAM BACK A LITTLE!?!?"):
+													champ = "Janna"
+											else:
+												if yes("If your ultimate were to stun a bunch of enemies, would you want to have to wait 3 seconds?"):
+													champ = "Morgana"
+												else:
+													champ = "Sona"
+						else:
+							champ = "Karma"
+					else:
+						if yes("You like getting bonus range, damage, and AOE on your basic attacks, becoming an unkillable late-game god?"):
+							champ = "Kayle"
+						else:
+							if yes("You like kickin' shit?"):
+								champ = "Lee Sin"
+							else:
+								if yes("Are you trying to fuck people up at melee range?"):
+										champ = "Shen"
+								else:
+									if yes("Are you a creepy clock guy?"):
+										champ = "Zilean"
+									else:
+										if yes("Are you horn-y?"):
+											champ = "Soraka"
+										else:
+											champ = "Lulu"
+			else:
+				if yes("Do you need to be ranged vs melee to win your lane?"):
+					if yes("Do you want to suck even after winning lane?"):
+						if yes("Is it hammer time?"):
+							champ = "Jayce"
+						else:
+							champ = "Kennen"
+					else:
+						if yes("Do leavers make you...MEGA upset?"):
+							champ = "Gnar"
+						else:
+							champ = "Quinn"
+				else:
+					if yes("Are you a hooker?"):
+						if yes("But are you more than just a hooker?"):
+							if yes("Are you trying to get 20 kills as a huge 'fuck you' to your noob ADC?"):
+								champ = "Pyke"
+							else:
+								if yes("Do you know what's in The Box?"):
+									champ = "Thresh"
+								else:
+									champ = "Nautilus"
+						else:
+							champ = "Blitzcrank"
+					else:
+						if yes("Do you think 'ammo' is a 'fun' game mechanic?"):
+							if yes("Do you think gaining temporary armor is a fun ability?"):
+								champ = "Graves"
+							else:
+								champ = "Jhin"
+						else:
+							if yes("Is 'sustained DPS' a panty-dropper for you?"):
+								if yes("We're talkin' sustained magic damage, right?"):
+									if yes("And snakes in bras?"):
+										champ = "Cassiopoeia"
+									else:
+										champ = "Azir"
+								else:
+									if yes("Are you so bad at skillshots that you need one that pierces through everything?"):
+										if yes("Defensive abilities are for pussies?"):
+											if yes("So something good happens if you accidentally die?"):
+												champ = "Kog'Maw"
+											else:
+												if yes("You're gonna lose all your money when you die, aren't you?"):
+													champ = "Draven"
+												else:
+													champ = "Varus"
+										else:
+											if yes("Is it your first time playing League of Legends?"):
+												if yes("Is it your first time playing any video game?"):
+													champ = "Caitlyn"
+												else:
+													champ = "Sivir"
+											else:
+												champ = "Xayah"
+									else:
+										if yes("Global ults, am I right?"):
+											if yes("And spamming skillshots?"):
+												champ = "Ezreal"
+											else:
+												if yes("Are chompy bois?"):
+													champ = "Jinx"
+												else:
+													champ = "Ashe"
+										else:
+											if yes("Okay, but you want some ultimate right?"):
+												if yes("Like a really, really weird one that nobody understands?"):
+													champ = "Kalista"
+												else:
+													if yes("And this whole time you were in the jungle?"):
+														champ = "Kindred"
+													else:
+														if yes("Do you like boobs?"):
+															if yes("Crazy large boobs?"):
+																champ = "Miss Fortune"
+															else:
+																champ = "Kai'sa"
+														else:
+															if yes("Do you want like, 5 different weapons and 2 spells or something?"):
+																champ = "Aphelios"
+															else:
+																if yes("Do you want to bounce around like a kangaroo on speed?"):
+																	champ = "Tristana"
+																else:
+																	champ = "Lucian"
+											else:
+												champ = "Corki"
+							else:
+								if yes("Does getting top damage from the mid lane make you hard?"):
+									if yes("and you did it with that weird champion that nobody has ever heard of, right?"):
+										champ = "Aurelion Sol"
+									else:
+										if yes("Can you deal with an ability that doesn't do damage?"):
+											if yes("Do you wanna be a spooky guy with a broken no-skill ultimate?"):
+												if yes("A short spooky guy?"):
+													champ = "Veigar"
+												else:
+													if yes("A skinny spooky guy?"):
+														champ = "Fiddlesticks"
+													else:
+														champ = "Karthus"
+											if yes("Do you want an ultimate that like, moves you somewhere else?"):
+												if yes("But like, right next to you?"):
+													champ = "Zoe"
+												else:
+													champ = "Taliyah"
+											else:
+												if yes("Is Articuno your favorite Pokemon?"):
+													champ = "Anivia"
+												if yes("Do you hate choosing which items to buy?"):
+														champ = "Viktor"
+												else:
+													if yes("Do you like playing dress-up?"):
+														champ = "Neeko"
+													else:
+														champ = "Zyra"
+										else:
+											if yes("Would Riot make a champion that has four damaging abilities and is tanky?"):
+												champ = "Swain"
+											if yes("You like point-and-click abilities, right?"):
+												if yes("But surely some of your abilities can be dodged?"):
+													if yes("Would you freeze yourself until the future comes if you could?"):
+														champ = "Lissandra"
+													else:
+														champ = "Brand"
+												else:
+													champ = "Syndra"
+											else:
+												if yes("You have strong feelings about turrets, either love or hate?"):
+													if yes("You love turrets?"):
+														champ = "Heimerdinger"
+													else:
+														champ = "Ziggs"
+												else:
+													if yes("Are you aiming for enemies that are on your screen?"):
+														champ = "Vel'koz"
+													else:
+														champ = "Xerath"
+								else:
+									if yes("Do you have 'Point-and-click CC' tattooed on your ass?"):
+										if yes("Do you at least want some damage?"):
+											if yes("Like teleporting?"):
+												if yes("Do you feel lucky, punk?"):
+													champ = "Twisted Fate"
+												else:
+													champ = "Ryze"
+											else:
+												if yes("Okay, but jumping to other lanes is basically teleporting, and you want to do that, right?"):
+													champ = "Pantheon"
+												else:
+													if yes ("Would Riot make a champion that has point-and-click CC AND damage AND some bullshit spell shield thing?"):
+														champ = "Malzahar"
+													else:
+														champ = "Annie"
+										else:
+											if yes("Is there also a royal flag up your ass?"):
+												champ = "Jarvan IV"
+											else:
+												champ = "Skarner"
+									else:
+										if yes("Is outplaying your opponent in fights the most important thing to you?"):
+											if yes("Does outplaying mean running directly at your opponent and spamming buttons?"):
+												if yes("Are you also kind of tanky, so you pretty much can't mess up spamming buttons?"):
+													champ = "Riven"
+												else:
+													if yes("Is one of the buttons you are spamming your opponent's ultimate?"):
+														champ = "Sylas"
+													else:
+														if yes("Does one of the buttons you are spamming at least give you some outplay potential?"):
+															if yes("And the sequencing of your abilities is important, right?"):
+																champ = "Qiyana"
+															else:
+																champ = "Talon"
+														else:
+															champ = "Diana"
+											if yes("Does outplaying mean using at least 3 dashes or teleports?"):
+												if yes("Do you want to be perma-banned?"):
+													if yes("And lose the games when you are not banned?"):
+														champ = "Yasuo"
+													else:
+														champ = "Zed"
+												else:
+													if yes("May we add some CC to your already high damage, high mobility champion?"):
+														if yes("May we add some tankiness as well, thus removing all weakness?"):
+															champ = "Irelia"
+														else:
+															if yes("Is your favorite movie 'The Prestige'?"):
+																champ = "LeBlanc"
+															else:
+																champ = "Ahri"
+													else:
+														if yes("May we add extreme scaling to your already high damage, high mobility champion?"):
+															champ = "Kassadin"
+														else:
+															if yes("Is picking up shit off the ground an enticing game mechanic?"):
+																champ = "Katarina"
+															else:
+																champ = "Akali"
+											else:
+												if yes("Does outplaying mean reversing time whenever you get low on health?"):
+													champ = "Ekko"
+												else:
+													if yes("Does outplaying your opoonent mean healing up to full as you pentakill the entire team?"):
+														champ = "Vladimir"		
+													else:
+														champ = "Fizz"			
+										else:
+											if yes("Do you need to be invisible to get a kill?"):
+												if yes("Do you also need to have copies of yourself to get a kill?"):
+													champ = "Shaco"
+												else:
+													if yes("Are mushrooms fucking amazing?"):
+														champ = "Teemo"
+													else: 
+														if yes("Do you like booooobs?"):
+															champ = "Evelynn"
+														else:
+															champ = "Twitch"
+											else:
+												if yes("Do you need to be underground to get a kill?"):
+													champ = "Rek'sai"
+												elif yes("Do you need to be on a icy coating of your own creation to get a kill?"):
+													champ = "Trundle"
+												else:
+													if yes("Do you want to snowball two kills in the toplane into an unstoppable pentakill machine?"):
+														if yes("And by pentakill machine, do you mean, tenta-kill machine?"):
+															champ = "Illaoi"
+														else:
+															if yes("Will any of your kills take place in a death arena of some sort?"):
+																if yes("A hexagonal one?"):
+																	champ = "Camille"
+																else:
+																	champ = "Mordekaiser"
+															else:
+																if yes("Do you like to stun people?"):
+																	if yes("And then pick them up, jump and piledrive them?"):
+																		champ = "Sett"
+																	else:
+																		champ = "Renekton"
+																else:
+																	if yes("Does winning require spinning?"):
+																		if yes("Are you surprised that there are two champions left? That is some similar shit."):
+																			champ = "Garen"
+																		else:
+																			champ = "Darius"
+																	else:
+																		if yes("Pharaoh doge?"):
+																			champ = "Nasus"
+																		else:
+																			champ = "Aatrox"
+													else:
+														if yes("Do you want to snowball two kills in the toplane into more solo kills and then eventually lose the game?"):
+															champ = "Fiora"
+														else:
+															if yes("Do you want a squishier, but high-damage top-laner?"):
+																if yes("And a barrel mini-game?"):
+																	champ = "Gangplank"
+																else:
+																	champ = "Rumble"
+															else:
+																if yes("Do you need to be able to run down all the bitches?"):
+																	if yes("And throwing shit at them?"):
+																		if yes("Do you like riding on some Monsters Inc shit?"):
+																			if yes("Would you rather ride on Sully from Monsters Inc than Randall from Monsters Inc?"):
+																				champ = "Nunu"
+																			else:
+																				champ = "Kled"
+																		else:
+																			if yes("Do you like being CC'ed?"):
+																				champ = "Rengar"
+																			else:
+																				champ = "Olaf"
+																	else:
+																		if yes("Are you a scythe-y boy?"):
+																			if yes("A scythe-y horse boy?"):
+																				champ = "Hecarim"
+																			else:
+																				champ = "Kayn"
+																		else:
+																			if yes("Might one find you at a Runeterra petting zoo?"):
+																				champ = "Warwick"
+																			else:
+																				if yes("While running people down, do you ever intend to attack them?"):
+																					if yes("And shake them like a polaroid picture?"):
+																						champ = "Rammus"
+																					else:
+																						champ = "Nocturne"
+																				else:
+																					champ = "Singed"
+																else:
+																	if yes("Does the word 'meatball' sound good to you?"):
+																		if yes("Do you want a big AoE ultimate?"):
+																			if yes("With you in the center?"):
+																				if yes("Do you like the color green?"):
+																					if yes("Do you like to be round?"):
+																						if yes("Do you like to be wrapped in toilet paper?"):
+																							champ = "Amumu"
+																						else:
+																							champ = "Zac"
+																					else:
+																						champ = "Galio"
+																				else:
+																					champ = "Malphite"	
+																			else:
+																				if yes("Do you want a hammer for a weapon?"):
+																					if yes("And a moustache?"):
+																						champ = "Ornn"
+																					else:
+																						champ = "Poppy"
+																				else:
+																					if yes("Do you want a sword for a weapon?"):
+																						champ = "Leona"
+																					else: 
+																						if yes("Do you want tree hands for a weapon?"):
+																							champ = "Maokai"
+																						else:
+																							champ = "Sion"
+																		else:
+																			if yes("Do you want a big single target damage ultimate?"):
+																				if yes("Do you like stealing Baron from the junglers?"):
+																					champ = "Cho'gath"
+																				else:
+																					if yes("Do you like to flip people over like a fuckin flapjack?"):
+																						champ = "Urgot"
+																					else:
+																						champ = "Sejuani"
+																			else:
+																				if yes("Do you want Dr. Mundo's ultimate?"):
+																					champ = "Dr. Mundo"
+																				else:
+																					champ = "Alistar"
+																	else:
+																		if yes("Did you complete the 'Pacifist' route in Undertale?"):
+																			champ = "Ivern"
+																		else:
+																			if yes("Do you want your champion to evolve like a Pokemon?"):
+																				champ = "Kha'zix"
+																			else:
+																				if yes("Do you want a dragon Animorph?"):
+																					champ = "Shyvana"
+																				else:
+																					if yes("Do you want a spider Animorph?"):
+																						champ = "Elise"
+																					else:
+																						if yes("Do you want a cougar Animorph?"):
+																							champ = "Nidalee"
+																						else:
+																							if yes("Do you want to be a monkey, just a monkey, not a monkey Animorph?"):
+																								champ = "Wukong"
+																							else:
+																								champ = "Gragas"	
+														
+		else: #No abilities
+			if yes("But you like to run really fast?"):
+				if yes("And then carry the late game?"):
+					if yes("Because you're a fuckin' man-bear-pig!?"):
+						champ = "Udyr"
+					else:
+						if yes("Because you were a Marksman the whole time, just with no abilities?"):
+							champ = "Vayne"
+						else:
+							champ = "Master Yi"
+				else:
+					champ = "Volibear"
+			else:
+				if yes("Do you love lamp?"):
+					champ = "Jax"
+				else:
+					champ = "Xin Zhao"
+				
+	else: #Don't want to play with others
+		if yes("Other ghouls?"):
+			champ = "Yorick"
+		else:
+			champ = "Tryndamere"
+
+	print("\n\nYour dream LoL champion is... " + champ + "!")
+	print("Type 's' to start over, 'q' to quit")
+	if input() == 'q':
+		break
---
+++ ChampFinder/README.txt
@@ -0,0 +1,6 @@
+
+1) Download and run ChampFinder.exe
+
+2) Examine the source code ChampFinder.py
+
+3) Send deedback to KazTown on twitch.tv/KazTown!
